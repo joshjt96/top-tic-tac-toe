397a005244bbfdaddfe7d9ce06870ddfdb6470a7
Josh Talley <jtdev84@gmail.com>
2022-07-21 23:11:46 +0100

    finish displayControllerModule

--- ttt.js
+++ ttt.js
@@ -19,8 +19,25 @@ const gameboardModule = (() => {
 })();
 
 // displayController Module
-const displayControllerModule = (() => {
-    displayController = [];
+const displayControllerModule = (function () {
+    const gridBtn = document.querySelectorAll('.board-button');
+    let index = 0;
+    gridBtn.forEach(gridBtn => {
+        gridBtn.dataset.linkedBtn = index;
+        gridBtn.addEventListener('click', renderArray);
+        function renderArray() {
+            let index = 0;
+            const grids = document.querySelectorAll('.grid');
+            grids.forEach(grids => {
+                grids.dataset.linkedBtn = index;
+                if (gridBtn.getAttribute('data-linked-btn') == grids.getAttribute('data-linked-btn')) { 
+                    grids.textContent = gameboardModule.gameboard; 
+                }
+                index++;
+            });
+        }
+    index++;
+    });
     return {};
 })();
 
@@ -31,11 +48,47 @@ const playerFactory = (turn, name) => {
     p2Input = document.getElementById('p2');
     p1Input.turn = 1;
     p2Input.turn = 2;
-    return {turn, name};
-
+    return { turn, name };
 };
+
+
 const p1 = playerFactory('1', 'Josh');
 const p2 = playerFactory('2', 'Amy');
+    // function renderGameboard() {
+    //     const grid = document.querySelector('.grid');
+    //     grid.addEventListener('click', () => {
+    //         grid.textContent = gameboard;
+    //         console.log('Click');
+    //     });
+    // };
+    // renderGameboard();
+    // function renderGameboard() {
+    //     const grids = document.querySelectorAll('.grid').forEach(function () {
+    //         grids.addEventListener('click', () => {
+    //             grids.textContent = gameboard;
+    //             console.log('Click');
+    //         });
+    //     });
+    //     renderGameboard();
+    // };
+    // let renderGameboardModule = (() => {
+    //     const grids = document.querySelectorAll('.grid');
+    //     grids.addEventListener('click', () => {
+    //         grids[0].textContent = "Test";
+    //         console.log('Click');
+    //     });
+    // });
+    // renderGameboardModule();
+    // function addPiece() {
+    //     const piece = document.querySelector('.grid');
+    //     piece.addEventListener('click', addX());
+    // };
+    // function addX() {
+    //     const piece = document.querySelector('.grid');
+    //     const add = document.createElement('h1')
+    //     add.textContent = 'X'
+    //     piece.appendChild()
+    // };
 
 // function renderGameboard() {
 //     const grid = document.querySelector('.grid');
